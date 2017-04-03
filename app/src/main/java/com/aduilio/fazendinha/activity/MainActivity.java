ba9bdc93f77ce6ce528ccb4dd4064bff39ff3a57
aduilio <developer.aduilio@gmail.com>
2017-04-03 13:51:56 -0300

    Play sounds

--- app/src/main/java/com/aduilio/fazendinha/activity/MainActivity.java
+++ app/src/main/java/com/aduilio/fazendinha/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aduilio.fazendinha.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -13,26 +16,45 @@ import com.aduilio.fazendinha.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer player;
+    private Vibrator vibrator;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
         ImageView porco = (ImageView) findViewById(R.id.iv_main_porco);
         porco.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                playSound(R.raw.porco);
             }
         });
 
-        ImageView carneiro = (ImageView) findViewById(R.id.iv_main_pato);
-        carneiro.setOnClickListener(new View.OnClickListener() {
+        ImageView pato = (ImageView) findViewById(R.id.iv_main_pato);
+        pato.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                playSound(R.raw.pato);
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        if (player != null && player.isPlaying()) {
+            player.stop();
+            player.release();
+        }
+
+        if (vibrator != null) {
+            vibrator.cancel();
+        }
+
+        super.onPause();
     }
 
     @Override
@@ -54,8 +76,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onVacaClick(View view) {
+        playSound(R.raw.vaca);
     }
 
     public void onPeruClick(View view) {
+        playSound(R.raw.peru);
+    }
+
+    private void playSound(int sound) {
+        player = MediaPlayer.create(this, sound);
+        player.start();
     }
 }
